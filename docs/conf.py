0bd310389e97f79991d80d3039de0077301cc10d
Joshua Fehler <jsfehler@gmail.com>
2021-12-21 14:50:32 -0500

    Fix broken import in docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 
 sys.path.insert(0, os.path.abspath('..'))
 
-import pytest_splinter
+import pytest_splinter4
 
 # -- General configuration -----------------------------------------------------
 
