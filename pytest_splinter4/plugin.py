4b5700dc59a8b0c83f8b5e3444349069154f9ebe
Joshua Fehler <jsfehler@gmail.com>
2024-06-10 17:56:40 -0400

    fixup

--- pytest_splinter4/plugin.py
+++ pytest_splinter4/plugin.py
@@ -283,8 +283,8 @@ def _splinter_driver_default_kwargs(splinter_logs_dir, splinter_remote_name):
         },
         'firefox': {
             'service': FirefoxService(
-                log_output=f"{splinter_logs_dir}/geckodriver.log",
-                service_args=['--log', 'debug']
+                #log_output=f"{splinter_logs_dir}/geckodriver.log",
+                #service_args=['--log', 'debug']
             ),
             'options': options['firefox'],
         },
